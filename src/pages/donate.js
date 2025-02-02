244110e226de1db6f5cb412b105304796eb3c0e9
Sajjad Dashti <saggad.dashti@gmail.com>
2025-02-02 16:50:24 +0400

    Fixing Donate meta and imports

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -1,9 +1,7 @@
 import * as React from "react"
-import { Link } from "gatsby"
 import Header from '../components/header';
 import Footer from '../components/footer';
 import Seo from '../components/seo'
-import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 
 
